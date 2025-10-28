10935198a13755f683522a42f077754cb4d7b712
Boris Gubanov <44787162+boris-gu@users.noreply.github.com>
2025-10-28 19:48:56 +0300

    Refactoring

--- main.c
+++ main.c
@@ -13,6 +13,13 @@
 #define TAKEOFF_ALT 3
 #define TAKEOFF_ALT_DELTA 0.5
 
+#define PERIOD_TX_DO_NOTHING_MS   500 
+#define PERIOD_TX_START_TASK_MS   5000
+#define PERIOD_TX_TO_GUIDED_MS    5000
+#define PERIOD_TX_ARM_MS          5000
+#define PERIOD_TX_TAKEOFF_MS      10000
+#define PERIOD_TX_FLY_TO_COORD_MS 2000
+
 typedef enum {
   TASK_DO_NOTHING,
   TASK_START_TASK,
@@ -28,6 +35,7 @@ typedef enum {
   CONNECT_UART
 } connection_t;
 
+int transmit_mav_msgs(connection_t con, mavlink_message_t *msg);
 void* mavlink_rx_thread(void* arg);
 void* mavlink_tx_thread(void* arg);
 
@@ -36,43 +44,22 @@ const int valid_baudrates[VALID_BAUDRATES_SIZE] = {110,   300,   600,    1200,  
                                                    4800,  9600,  14400,  19200,  38400,
                                                    56000, 57600, 115200, 128000, 256000};
 
-/**
- * СОЕДИНЕНИЯ
- */
+// СОЕДИНЕНИЯ
 connection_t con_type = CONNECT_NO_SELECT;
 // tcp
 WSADATA wsa;
 struct sockaddr_in server;
 SOCKET client_socket;
-u_long non_blocking_mode = 1;
 // uart
 HANDLE hSerial;
 
-/**
- * MAVLink
- */
+// MAVLink
 uint8_t drone_id = 0;
-uint8_t this_id = 254; // 255 - стандарт для наземных станций, мы будем использовать другой на всякий случай
 mavlink_heartbeat_t drone_hbeat = {};
 mavlink_global_position_int_t drone_pose = {};
 mavlink_statustext_t drone_text = {};
-int32_t target_lat = 584363792; // degE7
-int32_t target_lon = 309819603;
-float target_alt = 15; // m
 
-// Прием/Отправка
-uint8_t rx_buf [RX_BUFF_SIZE];
-DWORD rx_real_count = 0;
-mavlink_message_t rx_msg = {};
-mavlink_status_t rx_status = {};
-
-mavlink_message_t tx_msg = {};
-uint8_t tx_buf[MAVLINK_MAX_PACKET_LEN];
-uint16_t tx_len = 0;
-
-/**
- * STATE MACHINE
- */
+// STATE MACHINE
 task_t current_task = TASK_START_TASK;
 
 int main(int argc, char *argv[]) {
@@ -112,15 +99,6 @@ int main(int argc, char *argv[]) {
         return 1;
       }
       printf("Connection OK\n");
-
-      // Установка неблокирующего режима
-      // if (ioctlsocket(client_socket, FIONBIO, &non_blocking_mode) == SOCKET_ERROR) {
-      //   printf("Failed to set non-blocking mode. Error: %d\n", WSAGetLastError());
-      //   closesocket(client_socket);
-      //   WSACleanup();
-      //   return 1;
-      // }
-      // printf("Non-blocking mode OK\n");
       break;
     }
     case 3: {
@@ -260,15 +238,18 @@ int main(int argc, char *argv[]) {
       }
     }
   }
-
-  if (con_type == CONNECT_UART) {
-    CloseHandle(hSerial);
-  }
   return 0;
 }
 
 void* mavlink_rx_thread(void* arg) {
+  uint8_t rx_buf [RX_BUFF_SIZE];
+  DWORD rx_real_count = 0;
+  mavlink_message_t rx_msg = {};
+  mavlink_status_t rx_status = {};
   for (;;) {
+    /**
+     * Получаем данные
+     */
     if (con_type == CONNECT_TCP) {
       rx_real_count = recv(client_socket, rx_buf, RX_BUFF_SIZE - 1, 0);
       if (rx_real_count == SOCKET_ERROR) {
@@ -285,6 +266,9 @@ void* mavlink_rx_thread(void* arg) {
     } else {
       ReadFile(hSerial, rx_buf, RX_BUFF_SIZE, &rx_real_count, NULL);
     }
+    /**
+     * Парсинг
+     */
     for (int i = 0; i < rx_real_count; i++) {
       // Функция mavlink_parse_char пытается собрать пакет по одному байту, используя свой внутренний буфер
       if (mavlink_parse_char(0, rx_buf[i], &rx_msg, &rx_status)) {
@@ -343,10 +327,14 @@ void* mavlink_rx_thread(void* arg) {
 }
 
 void* mavlink_tx_thread(void* arg) {
+  uint8_t this_id = 254; // 255 - стандарт для наземных станций, мы будем использовать другой на всякий случай
+  // TODO: target_lat, target_lon, target_alt должны откуда-то обновляться
+  int32_t target_lat = 584363792; // degE7
+  int32_t target_lon = 309819603;
+  float target_alt = 15; // m
+  mavlink_message_t tx_msg = {};
   for (;;) {
-    // ЗАПИСЬ
     if (drone_id) {
-      // TODO: Отправка HEARTBEAT (и переписать для этого задержки Sleep())
       switch (current_task) {
         // 1. Начало выполнения задачи, запрашиваем сообщения
         case TASK_START_TASK: {
@@ -356,35 +344,20 @@ void* mavlink_tx_thread(void* arg) {
                                         MAVLINK_MSG_ID_GLOBAL_POSITION_INT,  // Что запрашиваем
                                         1000000,  // Интервал в микросекундах
                                         0, 0, 0, 0, 0);
-          tx_len = mavlink_msg_to_send_buffer(tx_buf, &tx_msg);
-          if (con_type == CONNECT_TCP) {
-            if (send(client_socket, tx_buf, tx_len, 0) == SOCKET_ERROR) {
-              printf("Send Req MAVLINK_MSG_ID_GLOBAL_POSITION_INT failed. Error: %d\n", WSAGetLastError());
-            }
-          } else {
-            WriteFile(hSerial, tx_buf, tx_len, NULL, NULL);
-          }
-          Sleep(5000);
+          transmit_mav_msgs(con_type, &tx_msg);
+          Sleep(PERIOD_TX_START_TASK_MS);
           break;
         }
         // 2. Переводим в режим Guided
         case TASK_TO_GUIDED: {
-          // if (drone_hbeat.base_mode & MAV_MODE_FLAG_SAFETY_ARMED && // Раскомментировать при реальных тестах
           printf("  SEND: COPTER_MODE_GUIDED\n");
           mavlink_msg_command_long_pack(this_id, MAV_COMP_ID_MISSIONPLANNER, &tx_msg,
                                         drone_id, MAV_COMP_ID_AUTOPILOT1,
                                         MAV_CMD_DO_SET_MODE , 0,
                                         MAV_MODE_FLAG_CUSTOM_MODE_ENABLED, COPTER_MODE_GUIDED,
                                         0, 0, 0, 0, 0);
-          tx_len = mavlink_msg_to_send_buffer(tx_buf, &tx_msg);
-          if (con_type == CONNECT_TCP) {
-            if (send(client_socket, tx_buf, tx_len, 0) == SOCKET_ERROR) {
-              printf("------- Send COPTER_MODE_GUIDED failed. Error: %d\n", WSAGetLastError());
-            }
-          } else {
-            WriteFile(hSerial, tx_buf, tx_len, NULL, NULL);
-          }
-          Sleep(5000);
+          transmit_mav_msgs(con_type, &tx_msg);
+          Sleep(PERIOD_TX_TO_GUIDED_MS);
           break;
         }
         // 3. Армим дрон
@@ -394,15 +367,8 @@ void* mavlink_tx_thread(void* arg) {
                                         drone_id, MAV_COMP_ID_AUTOPILOT1,
                                         MAV_CMD_COMPONENT_ARM_DISARM, 0,
                                         1, 0, 0, 0, 0, 0, 0); // 1 - ARM
-          tx_len = mavlink_msg_to_send_buffer(tx_buf, &tx_msg);
-          if (con_type == CONNECT_TCP) {
-            if (send(client_socket, tx_buf, tx_len, 0) == SOCKET_ERROR) {
-              printf("Send ARM failed. Error: %d\n", WSAGetLastError());
-            }
-          } else {
-            WriteFile(hSerial, tx_buf, tx_len, NULL, NULL);
-          }
-          Sleep(5000);
+          transmit_mav_msgs(con_type, &tx_msg);
+          Sleep(PERIOD_TX_ARM_MS);
           break;
         }
         // 4. Отправляем команды на взлет
@@ -412,15 +378,8 @@ void* mavlink_tx_thread(void* arg) {
                                         drone_id, MAV_COMP_ID_AUTOPILOT1,
                                         MAV_CMD_NAV_TAKEOFF, 0,
                                         0, 0, 0, 0, 0, 0, TAKEOFF_ALT);
-          tx_len = mavlink_msg_to_send_buffer(tx_buf, &tx_msg);
-          if (con_type == CONNECT_TCP) {
-            if (send(client_socket, tx_buf, tx_len, 0) == SOCKET_ERROR) {
-              printf("Send TAKEOFF failed. Error: %d\n", WSAGetLastError());
-            }
-          } else {
-            WriteFile(hSerial, tx_buf, tx_len, NULL, NULL);
-          }
-          Sleep(10000);
+          transmit_mav_msgs(con_type, &tx_msg);
+          Sleep(PERIOD_TX_TAKEOFF_MS);
           break;
         }
         // 5. Отправляем координаты
@@ -442,19 +401,25 @@ void* mavlink_tx_thread(void* arg) {
             POSITION_TARGET_TYPEMASK_YAW_IGNORE |
             POSITION_TARGET_TYPEMASK_YAW_RATE_IGNORE,
             target_lat, target_lon, target_alt, 0, 0, 0, 0, 0, 0, 0, 0);
-          tx_len = mavlink_msg_to_send_buffer(tx_buf, &tx_msg);
-          if (con_type == CONNECT_TCP) {
-            if (send(client_socket, tx_buf, tx_len, 0) == SOCKET_ERROR) {
-              printf("Send GLOBAL_POSITION_INT failed. Error: %d\n", WSAGetLastError());
-            }
-          } else {
-            WriteFile(hSerial, tx_buf, tx_len, NULL, NULL);
-          }
-          Sleep(2000);
+          transmit_mav_msgs(con_type, &tx_msg);
+          Sleep(PERIOD_TX_FLY_TO_COORD_MS);
           break;
         }
       }
     }
+  }
+  return 0;
+}
+
+int transmit_mav_msgs(connection_t con, mavlink_message_t *msg) {
+  uint8_t tx_buf[MAVLINK_MAX_PACKET_LEN];
+  uint16_t tx_len = mavlink_msg_to_send_buffer(tx_buf, msg);
+  if (con == CONNECT_TCP) {
+    if (send(client_socket, tx_buf, tx_len, 0) == SOCKET_ERROR) {
+      return WSAGetLastError();
+    }
+  } else {
+    WriteFile(hSerial, tx_buf, tx_len, NULL, NULL);
   }
   return 0;
 }
