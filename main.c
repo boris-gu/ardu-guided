c22b992b3de383c6c2ae9ba2c4121aa86c7452ce
Boris Gubanov <44787162+boris-gu@users.noreply.github.com>
2025-10-29 18:57:54 +0300

    Fix logic of tx delays

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 #define VALID_BAUDRATES_SIZE 15
 #define RX_BUFF_SIZE (MAVLINK_MAX_PACKET_LEN * 4)
 #define TAKEOFF_ALT 3
-#define TAKEOFF_ALT_DELTA 0.5
+#define TAKEOFF_ALT_DELTA 0.3
 
 #define PERIOD_TX_DO_NOTHING_MS   500 
 #define PERIOD_TX_START_TASK_MS   5000
@@ -35,9 +35,10 @@ typedef enum {
   CONNECT_UART
 } connection_t;
 
-int transmit_mav_msgs(connection_t con, mavlink_message_t *msg);
 void* mavlink_rx_thread(void* arg);
 void* mavlink_tx_thread(void* arg);
+int transmit_mav_msgs(connection_t con, mavlink_message_t *msg);
+void change_tx_period(DWORD *last_time, DWORD *period, int new_period);
 
 // Допустимые бодрейты прописаны в winbase.h через #define (например CBR_115200)
 const int valid_baudrates[VALID_BAUDRATES_SIZE] = {110,   300,   600,    1200,   2400,
@@ -61,6 +62,8 @@ mavlink_statustext_t drone_text = {};
 
 // STATE MACHINE
 task_t current_task = TASK_START_TASK;
+DWORD tx_mav_period = PERIOD_TX_START_TASK_MS;
+DWORD tx_mav_last_time = 0;
 
 int main(int argc, char *argv[]) {
   /**
@@ -195,6 +198,7 @@ int main(int argc, char *argv[]) {
         // Проверка, что получили GLOBAL_POSITION_INT
         if (drone_pose.time_boot_ms != 0) {
           current_task = TASK_TO_GUIDED;
+          change_tx_period(&tx_mav_last_time, &tx_mav_period, PERIOD_TX_TO_GUIDED_MS);
           printf("-- SM: TASK_TO_GUIDED\n");
         }  
         break;
@@ -204,8 +208,10 @@ int main(int argc, char *argv[]) {
         // путем перевода дрона в ручной режим Loiter
         if (drone_hbeat.custom_mode == COPTER_MODE_LOITER) {
           current_task = TASK_DO_NOTHING;
+          change_tx_period(&tx_mav_last_time, &tx_mav_period, PERIOD_TX_DO_NOTHING_MS);
         } else if (drone_hbeat.custom_mode == COPTER_MODE_GUIDED) {
           current_task = TASK_ARM;
+          change_tx_period(&tx_mav_last_time, &tx_mav_period, PERIOD_TX_ARM_MS);
           printf("-- SM: TASK_ARM\n");
         }
         break;
@@ -215,8 +221,10 @@ int main(int argc, char *argv[]) {
         // после перевода в ручной режим
         if (drone_hbeat.custom_mode != COPTER_MODE_GUIDED) {
           current_task = TASK_DO_NOTHING;
+          change_tx_period(&tx_mav_last_time, &tx_mav_period, PERIOD_TX_DO_NOTHING_MS);
         } else if (drone_hbeat.base_mode & MAV_MODE_FLAG_SAFETY_ARMED) {
           current_task = TASK_TAKEOFF;
+          change_tx_period(&tx_mav_last_time, &tx_mav_period, PERIOD_TX_TAKEOFF_MS);
           printf("-- SM: TASK_TAKEOFF\n");
         }
         break;
@@ -224,8 +232,10 @@ int main(int argc, char *argv[]) {
       case TASK_TAKEOFF: {
         if (drone_hbeat.custom_mode != COPTER_MODE_GUIDED) {
           current_task = TASK_DO_NOTHING;
+          change_tx_period(&tx_mav_last_time, &tx_mav_period, PERIOD_TX_DO_NOTHING_MS);
         } else if (fabs(TAKEOFF_ALT - (drone_pose.relative_alt / 1000.)) <= TAKEOFF_ALT_DELTA) {
            current_task = TASK_FLY_TO_COORD;
+           change_tx_period(&tx_mav_last_time, &tx_mav_period, PERIOD_TX_FLY_TO_COORD_MS);
           printf("-- SM: TASK_FLY_TO_COORD\n");
         }
         break;
@@ -233,6 +243,7 @@ int main(int argc, char *argv[]) {
       case TASK_FLY_TO_COORD: {
         if (drone_hbeat.custom_mode != COPTER_MODE_GUIDED) {
           current_task = TASK_DO_NOTHING;
+          change_tx_period(&tx_mav_last_time, &tx_mav_period, PERIOD_TX_DO_NOTHING_MS);
         }
         break;
       }
@@ -334,76 +345,75 @@ void* mavlink_tx_thread(void* arg) {
   float target_alt = 15; // m
   mavlink_message_t tx_msg = {};
   for (;;) {
-    if (drone_id) {
-      switch (current_task) {
-        // 1. Начало выполнения задачи, запрашиваем сообщения
-        case TASK_START_TASK: {
-          printf("  SEND: Req MAVLINK_MSG_ID_GLOBAL_POSITION_INT\n");
-          mavlink_msg_command_long_pack(this_id, MAV_COMP_ID_MISSIONPLANNER, &tx_msg,
-                                        drone_id, MAV_COMP_ID_AUTOPILOT1, MAV_CMD_SET_MESSAGE_INTERVAL, 0,
-                                        MAVLINK_MSG_ID_GLOBAL_POSITION_INT,  // Что запрашиваем
-                                        1000000,  // Интервал в микросекундах
-                                        0, 0, 0, 0, 0);
-          transmit_mav_msgs(con_type, &tx_msg);
-          Sleep(PERIOD_TX_START_TASK_MS);
-          break;
-        }
-        // 2. Переводим в режим Guided
-        case TASK_TO_GUIDED: {
-          printf("  SEND: COPTER_MODE_GUIDED\n");
-          mavlink_msg_command_long_pack(this_id, MAV_COMP_ID_MISSIONPLANNER, &tx_msg,
-                                        drone_id, MAV_COMP_ID_AUTOPILOT1,
-                                        MAV_CMD_DO_SET_MODE , 0,
-                                        MAV_MODE_FLAG_CUSTOM_MODE_ENABLED, COPTER_MODE_GUIDED,
-                                        0, 0, 0, 0, 0);
-          transmit_mav_msgs(con_type, &tx_msg);
-          Sleep(PERIOD_TX_TO_GUIDED_MS);
-          break;
-        }
-        // 3. Армим дрон
-        case TASK_ARM: {
-          printf("  SEND: ARM\n");
-          mavlink_msg_command_long_pack(this_id, MAV_COMP_ID_MISSIONPLANNER, &tx_msg,
-                                        drone_id, MAV_COMP_ID_AUTOPILOT1,
-                                        MAV_CMD_COMPONENT_ARM_DISARM, 0,
-                                        1, 0, 0, 0, 0, 0, 0); // 1 - ARM
-          transmit_mav_msgs(con_type, &tx_msg);
-          Sleep(PERIOD_TX_ARM_MS);
-          break;
-        }
-        // 4. Отправляем команды на взлет
-        case TASK_TAKEOFF: {
-          printf("  SEND: TAKEOFF\n");
-          mavlink_msg_command_long_pack(this_id, MAV_COMP_ID_MISSIONPLANNER, &tx_msg,
-                                        drone_id, MAV_COMP_ID_AUTOPILOT1,
-                                        MAV_CMD_NAV_TAKEOFF, 0,
-                                        0, 0, 0, 0, 0, 0, TAKEOFF_ALT);
-          transmit_mav_msgs(con_type, &tx_msg);
-          Sleep(PERIOD_TX_TAKEOFF_MS);
-          break;
-        }
-        // 5. Отправляем координаты
-        case TASK_FLY_TO_COORD: {
-          printf ("  SEND: Pos global lat:%d, lon:%d, alt:%f\n", target_lat, target_lon, target_alt);
-          mavlink_msg_set_position_target_global_int_pack(
-            this_id, MAV_COMP_ID_MISSIONPLANNER, &tx_msg,
-            0,  // time_boot_ms скорее всего можно оставить 0
-            drone_id, MAV_COMP_ID_AUTOPILOT1,
-            MAV_FRAME_GLOBAL_RELATIVE_ALT,  // Возможно другой фрейм
-            // Битовая маска для игнорирования всех команд, кроме позиции
-            // 0b110111111000
-            POSITION_TARGET_TYPEMASK_VX_IGNORE |
-            POSITION_TARGET_TYPEMASK_VY_IGNORE |
-            POSITION_TARGET_TYPEMASK_VZ_IGNORE |
-            POSITION_TARGET_TYPEMASK_AX_IGNORE |
-            POSITION_TARGET_TYPEMASK_AY_IGNORE |
-            POSITION_TARGET_TYPEMASK_AZ_IGNORE |
-            POSITION_TARGET_TYPEMASK_YAW_IGNORE |
-            POSITION_TARGET_TYPEMASK_YAW_RATE_IGNORE,
-            target_lat, target_lon, target_alt, 0, 0, 0, 0, 0, 0, 0, 0);
-          transmit_mav_msgs(con_type, &tx_msg);
-          Sleep(PERIOD_TX_FLY_TO_COORD_MS);
-          break;
+    DWORD current_time = GetTickCount();
+    if (current_time - tx_mav_last_time >= tx_mav_period) {
+      tx_mav_last_time = current_time;
+      if (drone_id) {
+        switch (current_task) {
+          // 1. Начало выполнения задачи, запрашиваем сообщения
+          case TASK_START_TASK: {
+            printf("  SEND: Req MAVLINK_MSG_ID_GLOBAL_POSITION_INT\n");
+            mavlink_msg_command_long_pack(this_id, MAV_COMP_ID_MISSIONPLANNER, &tx_msg,
+                                          drone_id, MAV_COMP_ID_AUTOPILOT1, MAV_CMD_SET_MESSAGE_INTERVAL, 0,
+                                          MAVLINK_MSG_ID_GLOBAL_POSITION_INT,  // Что запрашиваем
+                                          1000000,  // Интервал в микросекундах
+                                          0, 0, 0, 0, 0);
+            transmit_mav_msgs(con_type, &tx_msg);
+            break;
+          }
+          // 2. Переводим в режим Guided
+          case TASK_TO_GUIDED: {
+            printf("  SEND: COPTER_MODE_GUIDED\n");
+            mavlink_msg_command_long_pack(this_id, MAV_COMP_ID_MISSIONPLANNER, &tx_msg,
+                                          drone_id, MAV_COMP_ID_AUTOPILOT1,
+                                          MAV_CMD_DO_SET_MODE , 0,
+                                          MAV_MODE_FLAG_CUSTOM_MODE_ENABLED, COPTER_MODE_GUIDED,
+                                          0, 0, 0, 0, 0);
+            transmit_mav_msgs(con_type, &tx_msg);
+            break;
+          }
+          // 3. Армим дрон
+          case TASK_ARM: {
+            printf("  SEND: ARM\n");
+            mavlink_msg_command_long_pack(this_id, MAV_COMP_ID_MISSIONPLANNER, &tx_msg,
+                                          drone_id, MAV_COMP_ID_AUTOPILOT1,
+                                          MAV_CMD_COMPONENT_ARM_DISARM, 0,
+                                          1, 0, 0, 0, 0, 0, 0); // 1 - ARM
+            transmit_mav_msgs(con_type, &tx_msg);
+            break;
+          }
+          // 4. Отправляем команды на взлет
+          case TASK_TAKEOFF: {
+            printf("  SEND: TAKEOFF\n");
+            mavlink_msg_command_long_pack(this_id, MAV_COMP_ID_MISSIONPLANNER, &tx_msg,
+                                          drone_id, MAV_COMP_ID_AUTOPILOT1,
+                                          MAV_CMD_NAV_TAKEOFF, 0,
+                                          0, 0, 0, 0, 0, 0, TAKEOFF_ALT);
+            transmit_mav_msgs(con_type, &tx_msg);
+            break;
+          }
+          // 5. Отправляем координаты
+          case TASK_FLY_TO_COORD: {
+            printf ("  SEND: Pos global lat:%d, lon:%d, alt:%f\n", target_lat, target_lon, target_alt);
+            mavlink_msg_set_position_target_global_int_pack(
+              this_id, MAV_COMP_ID_MISSIONPLANNER, &tx_msg,
+              0,  // time_boot_ms скорее всего можно оставить 0
+              drone_id, MAV_COMP_ID_AUTOPILOT1,
+              MAV_FRAME_GLOBAL_RELATIVE_ALT,  // Возможно другой фрейм
+              // Битовая маска для игнорирования всех команд, кроме позиции
+              // 0b110111111000
+              POSITION_TARGET_TYPEMASK_VX_IGNORE |
+              POSITION_TARGET_TYPEMASK_VY_IGNORE |
+              POSITION_TARGET_TYPEMASK_VZ_IGNORE |
+              POSITION_TARGET_TYPEMASK_AX_IGNORE |
+              POSITION_TARGET_TYPEMASK_AY_IGNORE |
+              POSITION_TARGET_TYPEMASK_AZ_IGNORE |
+              POSITION_TARGET_TYPEMASK_YAW_IGNORE |
+              POSITION_TARGET_TYPEMASK_YAW_RATE_IGNORE,
+              target_lat, target_lon, target_alt, 0, 0, 0, 0, 0, 0, 0, 0);
+            transmit_mav_msgs(con_type, &tx_msg);
+            break;
+          }
         }
       }
     }
@@ -424,3 +434,7 @@ int transmit_mav_msgs(connection_t con, mavlink_message_t *msg) {
   return 0;
 }
 
+void change_tx_period(DWORD *last_time, DWORD *period,int new_period) {
+  *last_time = 0; // Сброс для быстрого перехода к следующему состоянию
+  *period = new_period;
+}
